26686884bb8c047d47f41c8c5c4f3e525c3c4b17
abyssalCodewright <zerosie@gmail.com>
2024-08-28 09:47:15 -0500

    LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -3,10 +3,12 @@ class HTMLNode:
         self.tag = tag
         self.value = value
         self.children = children
-        self.props = props
-
+        self.props = props   
+    
+    
     def to_html(self):
-        raise NotImplementedError("to_html not implemented")
+        raise NotImplementedError("to_html not implemented")    
+    
     
     def props_to_html(self):
         if self.props is None:
@@ -14,7 +16,29 @@ class HTMLNode:
         props_html = ""
         for prop in self.props:
             props_html += f' {prop}="{self.props[prop]}"'
-        return props_html
-        
+        return props_html    
+    
+    
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All LeafNodes must have a value")
+           
+        if self.tag is None or self.tag == "":
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr_works(self):
@@ -7,20 +8,24 @@ class TestHTMLNode(unittest.TestCase):
         expect = "HTMLNode(p, This is a paragraph., children: None, {'href': 'www.blizzard.com'})"
         self.assertEqual(repr(node), expect)
 
+
     def test_props_to_html_works(self):
         props = HTMLNode("p", "text string", None, {"href": "www.blizzard.com"})
         expect = ' href="www.blizzard.com"'
         self.assertEqual(props.props_to_html(), expect)
+
 
     def test_props_to_html_twice(self):
         props = HTMLNode("p", "text string", None, {"href": "www.blizzard.com", "target": "_blank"})
         expect = ' href="www.blizzard.com" target="_blank"'
         self.assertEqual(props.props_to_html(), expect)
 
+
     def test_all_none(self):
         node = HTMLNode()
         expect = "HTMLNode(None, None, children: None, None)"
         self.assertEqual(repr(node), expect)
+
 
     def test_values(self):
         node = HTMLNode("div", "Eff you, I can read and write, too.")
@@ -28,6 +33,36 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node.value, "Eff you, I can read and write, too.")
         self.assertEqual(node.children, None)
         self.assertEqual(node.props, None)
+
+
+    def test_value_is_none(self):
+        with self.assertRaises(ValueError) as context:
+            LeafNode("p", None).to_html()
+        self.assertEqual(str(context.exception), "All LeafNodes must have a value")
+
+
+    def test_tag_is_none(self):
+        node = LeafNode(None, "sentence here")
+        expect = "sentence here"
+        self.assertEqual(node.to_html(), expect)
+
+
+    def test_link(self):
+        node = LeafNode("a", "blizzard", {"website": "www.blizzard.com"})
+        enode = '<a website="www.blizzard.com">blizzard</a>'
+        self.assertEqual(node.to_html(), enode)
+
+
+    def test_paragraph(self):
+        node = LeafNode("p", "This is a paragraph")
+        enode = "<p>This is a paragraph</p>"
+        self.assertEqual(node.to_html(), enode)
+
+
+    def test_bold(self):
+        node = LeafNode("b", "This text is bold")
+        enode = "<b>This text is bold</b>"
+        self.assertEqual(node.to_html(), enode)
 
 
 
