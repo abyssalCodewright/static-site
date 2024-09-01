dbc265488c35604ab3f0e949be7b2db66b6fa6b6
abyssalCodewright <zerosie@gmail.com>
2024-09-01 10:04:04 -0500

    finished mkdn stuff!

--- src/test_mkdn_into_txtnode.py
+++ src/test_mkdn_into_txtnode.py
@@ -1,9 +1,11 @@
-from mkdn_into_txtnode import split_nodes_delimiter
+from mkdn_into_txtnode import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import (
     text_type_bold,
     text_type_code,
     text_type_italic,
     text_type_text,
+    text_type_image,
+    text_type_link,
     TextNode
 )
 import unittest
@@ -55,4 +57,71 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode("italic", text_type_italic),
             ],
             new_nodes,
+        )
+
+
+    def test_extract_images(self):
+        words = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(words), [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+
+
+    def test_extract_images_with_less_punc(self):
+        words = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(words), [('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+
+
+    def test_extract_links(self):
+        words = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_links(words), [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+
+    def test_extract_links_with_punc(self):
+        words = "This is text with a link ![to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_links(words), [("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+
+    def test_split_image_single(self):
+            node = TextNode("This is text with a image ![to boot dev](https://www.boot.dev)", text_type_text)
+            self.assertListEqual(
+                [
+                    TextNode("This is text with a image ", text_type_text),
+                    TextNode("to boot dev", text_type_image, "https://www.boot.dev")
+                ],
+                split_nodes_image([node])
+            )
+
+
+    def test_split_image_just_image(self):
+        node = TextNode("![to boot dev](https://www.boot.dev)", text_type_text)
+        self.assertListEqual(
+            [
+                TextNode("to boot dev", text_type_image, "https://www.boot.dev")
+            ],
+            split_nodes_image([node])
+        )
+
+
+    def test_split_image_multiple(self):
+        node = TextNode("This is text with a image ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)", text_type_text)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a image ", text_type_text),
+                TextNode("to boot dev", text_type_image, "https://www.boot.dev"),
+                TextNode(" and ", text_type_text),
+                TextNode("to youtube", text_type_image, "https://www.youtube.com/@bootdotdev")
+            ],
+            split_nodes_image([node])
+        )
+
+
+    def test_split_link(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", text_type_text)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", text_type_text),
+                TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+                TextNode(" and ", text_type_text),
+                TextNode("to youtube", text_type_link, "https://www.youtube.com/@bootdotdev")
+            ],
+            split_nodes_link([node])
         )
