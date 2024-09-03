7eb6463c90351a683b23db3b2b413b15299e7da5
abyssalCodewright <zerosie@gmail.com>
2024-09-03 14:15:04 -0500

    blocktype and tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,11 @@
+block_type_paragraph = "paragraph"
+block_type_code = "code"
+block_type_heading = "heading"
+block_type_quote = "quote"
+block_type_ulist = "unordered_list"
+block_type_olist = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     lines = markdown.split("\n\n")
     blocks = []
@@ -9,3 +17,37 @@ def markdown_to_blocks(markdown):
         blocks.append(line)
 
     return blocks
+
+
+def block_to_block_type(markdown_block):
+    lines = markdown_block.split("\n")
+    
+    if (
+        markdown_block.startswith("# ")
+        or markdown_block.startswith("## ")
+        or markdown_block.startswith("### ")
+        or markdown_block.startswith("#### ")
+        or markdown_block.startswith("##### ")
+        or markdown_block.startswith("###### ")
+    ):
+        return block_type_heading
+        
+    if all(line.startswith(">") for line in lines):
+        return block_type_quote
+    if all(line.startswith("* ") for line in lines):
+        return block_type_ulist
+    if all(line.startswith("- ") for line in lines):
+        return block_type_ulist
+    
+    if markdown_block.startswith("```") and markdown_block.endswith("```"):
+        return block_type_code
+    
+    if markdown_block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+        
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,13 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
+from markdown_blocks import (
+    block_type_olist,
+    block_type_code,
+    block_type_heading,
+    block_type_paragraph,
+    block_type_quote,
+    block_type_ulist
+)
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_does_it_work(self):
@@ -27,3 +35,69 @@ This is a paragraph."""
         node = markdown_to_blocks(mdown)
         enode = ["# This is a heading", "This is a paragraph."]
         self.assertListEqual(node, enode)
+
+
+    def test_blocktype_heading(self):
+        script = "### This is a heading of three"
+        self.assertEqual(block_to_block_type(script), block_type_heading)
+
+
+    def test_blocktype_code(self):
+        script = "```This is a code block.```"
+        self.assertEqual(block_to_block_type(script), block_type_code)
+
+
+    def test_blocktype_quote(self):
+        script = ">This is a quote"
+        self.assertEqual(block_to_block_type(script), block_type_quote)
+
+
+    def test_blocktype_paragraph(self):
+        script = "Ain't nobody here but us paragraphs."
+        self.assertEqual(block_to_block_type(script), block_type_paragraph)
+
+
+    def test_blocktype_unordered_list(self):
+        script = """
+* This is an unordered list with an asterisk
+* Another item in list
+
+- This is an unordered list with a dash
+- Another item in this list"""
+        node = markdown_to_blocks(script)
+        asterisk_block = block_to_block_type(node[0])
+        dash_block = block_to_block_type(node[1])
+        self.assertEqual(asterisk_block, block_type_ulist)
+        self.assertEqual(dash_block, block_type_ulist)
+
+
+    def test_blocktype_ordered_list(self):
+        script = """
+1. first item
+2. second item
+3. third item"""
+        node = markdown_to_blocks(script)
+        self.assertEqual(block_to_block_type(node[0]), block_type_olist)
+
+
+    def test_not_heading_blocktype(self):
+        script = "######## This is an incorrect heading"
+        self.assertEqual(block_to_block_type(script), block_type_paragraph)
+
+
+    def test_not_quote_blocktype(self):
+        script = """
+>This is the beginning of the quote
+but this line is wrong
+>so this should be a paragraph"""
+        node = markdown_to_blocks(script)
+        self.assertEqual(block_to_block_type(node[0]), block_type_paragraph)
+
+
+    def test_not_ordered_list_blocktype(self):
+        script = """
+1. this is number one
+5. this is not number two
+3. this is number 3"""
+        node = markdown_to_blocks(script)
+        self.assertEqual(block_to_block_type(node[0]), block_type_paragraph)
